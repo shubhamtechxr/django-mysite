e259f06e4533a3b290dc58ee25ce7d9014439441
Shubham Gupta <shubham.techxr@gmail.com>
2022-01-17 00:43:33 +0530

    6th lec

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello shubham")
+    return HttpResponse('''<h1>TechXR</h1> <a href="https://www.techxr.co/">mysite</a>''')
 
 def about(request):
     return HttpResponse("about shubham")    
