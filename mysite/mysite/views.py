2187f339a07ce7525df0e76eb930acec323a7d96
Shubham Gupta <shubham.techxr@gmail.com>
2022-01-17 00:57:12 +0530

    7th lec

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,26 @@
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('''<h1>TechXR</h1> <a href="https://www.techxr.co/">mysite</a>''')
+# def index(request):
+#     return HttpResponse('''<h1>TechXR</h1> <a href="https://www.techxr.co/">mysite</a>''')
 
-def about(request):
-    return HttpResponse("about shubham")    
+# def about(request):
+#     return HttpResponse("about shubham")    
+
+def index(request):
+    return HttpResponse("Home")
+
+def removepunc(request):
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("capitalize first")
+
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+def charcount(request):
+    return HttpResponse("charcount ")
