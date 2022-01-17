adae7965fcbb069fc02232756ce62857a073979e
Shubham Gupta <shubham.techxr@gmail.com>
2022-01-17 15:30:24 +0530

    12 lec complete website

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -15,6 +15,11 @@ def index(request):
 def analyze(request):
     djtext=request.GET.get('text','default')
     removepunc=request.GET.get('removepunc','off')
+    fullcaps=request.GET.get('fullcaps','off')
+    newlineremover=request.GET.get('newlineremover','off')
+    extraspaceremover=request.GET.get('extraspaceremover','off')
+    countchar=request.GET.get('countchar','off')
+
 
     if removepunc == 'on':
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -24,6 +29,41 @@ def analyze(request):
                 analyzed=analyzed+char
         params={'purpose': 'Remove Punctuations','analyzed_text':analyzed}
         return render(request,'analyze.html',params)
+
+    elif(fullcaps=='on'):
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+
+        params= {'purpose':'Changed to uppercase','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)    
+
+    elif(newlineremover=='on'):
+        analyzed=""
+        for char in djtext:
+            if char!='\n':
+                analyzed+=char
+
+        params= {'purpose':'Removed newlines','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)   
+
+    elif(extraspaceremover=='on'):
+        analyzed=""
+        for index,char in enumerate(djtext):
+            if not(djtext[index]==" "and djtext[index+1]==" "):
+                analyzed+=char
+
+        params= {'purpose':'Removed extraspace','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+    elif(countchar=='on'):
+        count=0
+        for char in djtext:
+            count=count+1
+
+        params= {'purpose':'Count total char','analyzed_text':count}
+        return render(request,'analyze.html',params)   
+                       
 
     else:
         return HttpResponse('Error')        
