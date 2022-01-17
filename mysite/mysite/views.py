a57ba526fcd7960a9f292a8fe3c5fcedaa782827
Shubham Gupta <shubham.techxr@gmail.com>
2022-01-17 12:59:41 +0530

    10th lec

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -9,25 +9,21 @@ from django.shortcuts import render
 #     return HttpResponse("about shubham")    
 
 def index(request):
-    #Get the text
-    djtext = request.GET.get('text', 'default')
-    print(djtext)
-    #Analyze the text
     return render(request, 'index.html')
-    # return HttpResponse("Home")
+  
 
-def removepunc(request):
-    return HttpResponse("remove punc")
+def analyze(request):
+    djtext=request.GET.get('text','default')
+    removepunc=request.GET.get('removepunc','off')
 
-def capfirst(request):
-    return HttpResponse("capitalize first")
+    if removepunc == 'on':
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed=""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed=analyzed+char
+        params={'purpose': 'Remove Punctuations','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
 
-def newlineremove(request):
-    return HttpResponse("capitalize first")
-
-
-def spaceremove(request):
-    return HttpResponse("space remover")
-
-def charcount(request):
-    return HttpResponse("charcount ")
+    else:
+        return HttpResponse('Error')        
