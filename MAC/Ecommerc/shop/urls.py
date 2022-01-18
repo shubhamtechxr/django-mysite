ddc1a68fa64fb530e0b3899e5f224c8a9d9ff8cc
Shubham Gupta <shubham.techxr@gmail.com>
2022-01-18 19:55:43 +0530

    till 27 lect

--- MAC/Ecommerc/shop/urls.py
+++ MAC/Ecommerc/shop/urls.py
@@ -3,4 +3,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.index, name="ShopHome"),
+path('about/', views.about, name="about"),
+path('contact/', views.contact, name="contact"),
+path('tracker/', views.tracker, name="tracker"),
+path('search/', views.search, name="search"),
+path('prodview/', views.prodview, name="prodview"),
+path('checkout/', views.checkout, name="checkout"),
 ]
