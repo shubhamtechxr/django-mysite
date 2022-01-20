dcc3157d922badb940629d434e34fb9beeb93640
Shubham Gupta <shubham.techxr@gmail.com>
2022-01-21 01:35:00 +0530

    Rest framework

---
+++ myrest/webapp/admin.py
@@ -0,0 +1,8 @@
+from atexit import register
+from django.contrib import admin
+from . models import employees
+
+admin.site.register(employees)
+
+
+# Register your models here.
---
+++ myrest/webapp/views.py
@@ -0,0 +1,23 @@
+import imp
+from urllib import response
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import employees
+from . serilizers import employeesSerializer
+
+class employeeList(APIView):
+
+    def get(self,request):
+        emp1=employees.objects.all()
+        serializer=employeesSerializer(emp1,many=True)
+        return Response(serializer.data)
+
+    def post(self ):
+        pass    
+
+
