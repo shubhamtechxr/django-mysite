fd496aa6e9369ff5d1aa240d43da9a6dcc308405
Shubham Gupta <shubham.techxr@gmail.com>
2022-01-20 19:25:07 +0530

    till 40th lec

--- MAC/Ecommerc/shop/views.py
+++ MAC/Ecommerc/shop/views.py
@@ -1,22 +1,34 @@
+from itertools import product
+from .models import Product
 from django.shortcuts import render, HttpResponse
-
+from math import ceil
 def index(request):
-    return render(request,'shop/index.html')
+    allprods=[]
+    catprods=Product.objects.values('category','id')
+    cats= {item['category'] for item in catprods}
+    for cat in cats:
+        prod =Product.objects.filter(category=cat)
+        n=len(prod)
+        nSlides= n//4+ceil((n/4)+n//4)
+        allprods.append([prod,range(1,nSlides),nSlides])
 
+    params={'allProds':allprods }
+    return render(request,'shop/index.html',params)
+  
 def about(request):
-    return HttpResponse("We are at about")
+    return render(request,'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")   
+    return render(request,'shop/contact.html')   
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request,'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request,'shop/search.html')
 
 def prodview(request):
-    return HttpResponse("We are at prodviews")
+    return render(request,'shop/prodview.html')
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request,'shop/checkout.html')
